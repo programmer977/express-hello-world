a187d3c4aa1dfc7536ff99ae4a6f2250e1756955
programmer977 <joaomarceloalvesdecavalho@gmail.com>
2025-11-24 15:51:43 -0300

    Add endpoint to return JSON response for /list/:id

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
-
+app.get("/list/:id", (req, response) => {
+  response.json({hello: "world"})
+});
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
